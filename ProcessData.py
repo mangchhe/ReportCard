1499ff5606d3c4d2160265fd166b0cefb50faec1
HaJooHyun <mangchhe@naver.com>
2020-01-13 01:39:34 +0900

    ProcessData.py 수정 [매 학기 평균 학점 계산, 해당 학점에 따른 계산법[미구현]]

--- ProcessData.py
+++ ProcessData.py
@@ -1,9 +1,51 @@
 from ExcelData import ExcelData
 
-if __name__ == '__main__':
+def getAvg(semester):
 
     excelData = ExcelData()
 
-    excelData.getRank()
-    excelData.getCredit()
-    excelData.getCourseName()
+    gradeRank = []
+    selectGrade = semester
+
+    tempGradeName = excelData.getCourseName()[selectGrade]
+
+    valueKo = []
+
+    for i in excelData.getRank()[selectGrade]:
+        valueKo.append(tempGradeName[0])
+        del tempGradeName[0]
+
+        if i == 'A+':
+            gradeRank.append(4.5)
+        elif i == 'A0':
+            gradeRank.append(4.0)
+        elif i == 'B+':
+            gradeRank.append(3.5)
+        elif i == 'B0':
+            gradeRank.append(3.0)
+        elif i == 'C+':
+            gradeRank.append(2.5)
+        elif i == 'C0':
+            gradeRank.append(2.0)
+        elif i == 'D+':
+            gradeRank.append(1.5)
+        elif i == 'D0':
+            gradeRank.append(1.0)
+        elif i == 'F':
+            gradeRank.append(0.0)
+        elif i == 'P':
+            del valueKo[-1]
+            pass
+        else:
+            pass
+
+    sum = 0
+    gradeRankNum = len(gradeRank)
+    for i in range(gradeRankNum):
+        sum += gradeRank.pop()
+
+    return round(sum/gradeRankNum,2)
+
+for i in range(0,6,1):
+    print(getAvg(i))
+

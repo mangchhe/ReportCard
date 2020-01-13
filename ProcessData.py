8351572351726a212c6ccedb24bc6eed2e4cead5
HaJooHyun <mangchhe@naver.com>
2020-01-13 12:42:13 +0900

    ProcessData.py 수정 [클래스로 변경] ReportCardMain.py 수정 [ProcessData 객체 참조하여 매 학기 평균 학점 불러와 plot으로 출력]

--- ProcessData.py
+++ ProcessData.py
@@ -1,51 +1,55 @@
 from ExcelData import ExcelData
 
-def getAvg(semester):
+class ProcessData:
 
-    excelData = ExcelData()
+    def __init__(self):
+        self.excelData = ExcelData()
 
-    gradeRank = []
-    selectGrade = semester
+    def getAvg(self, semester):
 
-    tempGradeName = excelData.getCourseName()[selectGrade]
+        gradeRank = []
+        selectGrade = semester
 
-    valueKo = []
+        tempGradeName = self.excelData.getCourseName()[selectGrade]
 
-    for i in excelData.getRank()[selectGrade]:
-        valueKo.append(tempGradeName[0])
-        del tempGradeName[0]
+        valueKo = []
 
-        if i == 'A+':
-            gradeRank.append(4.5)
-        elif i == 'A0':
-            gradeRank.append(4.0)
-        elif i == 'B+':
-            gradeRank.append(3.5)
-        elif i == 'B0':
-            gradeRank.append(3.0)
-        elif i == 'C+':
-            gradeRank.append(2.5)
-        elif i == 'C0':
-            gradeRank.append(2.0)
-        elif i == 'D+':
-            gradeRank.append(1.5)
-        elif i == 'D0':
-            gradeRank.append(1.0)
-        elif i == 'F':
-            gradeRank.append(0.0)
-        elif i == 'P':
-            del valueKo[-1]
-            pass
-        else:
-            pass
+        for i in self.excelData.getRank()[selectGrade]:
+            valueKo.append(tempGradeName[0])
+            del tempGradeName[0]
 
-    sum = 0
-    gradeRankNum = len(gradeRank)
-    for i in range(gradeRankNum):
-        sum += gradeRank.pop()
+            if i == 'A+':
+                gradeRank.append(4.5)
+            elif i == 'A0':
+                gradeRank.append(4.0)
+            elif i == 'B+':
+                gradeRank.append(3.5)
+            elif i == 'B0':
+                gradeRank.append(3.0)
+            elif i == 'C+':
+                gradeRank.append(2.5)
+            elif i == 'C0':
+                gradeRank.append(2.0)
+            elif i == 'D+':
+                gradeRank.append(1.5)
+            elif i == 'D0':
+                gradeRank.append(1.0)
+            elif i == 'F':
+                gradeRank.append(0.0)
+            elif i == 'P':
+                del valueKo[-1]
+                pass
+            else:
+                pass
 
-    return round(sum/gradeRankNum,2)
+        sum = 0
+        gradeRankNum = len(gradeRank)
+        for i in range(gradeRankNum):
+            sum += gradeRank.pop()
 
+        return round(sum/gradeRankNum,2)
+
+processData = ProcessData()
 for i in range(0,6,1):
-    print(getAvg(i))
+    print(processData.getAvg(i))
 

262d5851ab2c2f2567dc380f9e4b76b1d89a0ff6
HaJooHyun <mangchhe@naver.com>
2020-01-16 00:14:36 +0900

    ReportCardMain.py 수정 [학점 클릭 이벤트 처리 구현]

--- reportCardMain.py
+++ reportCardMain.py
@@ -13,6 +13,17 @@ font3 = {'family' : 'Hancom Gothic', 'size' : 12, 'color' : 'blue'}
 plt.rcParams['font.family'] = 'Hancom Gothic'   # 폰트 설정
 plt.rcParams['font.size'] = 8                   # 크기 설정
 
+excelData = ExcelData()
+processData = ProcessData()
+
+zum = []
+
+def onclick(event):
+    for i in range(excelData.getGradeCount()):
+        if (event.xdata < i + 0.1 and event.xdata > i - 0.1) and (event.ydata < zum[i] + 0.1 and event.ydata > zum[i] - 0.1):
+            print(i)
+
+
 if __name__ == '__main__':
 
     fig = plt.figure(figsize=(10,6)) # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.figure.html
@@ -23,10 +34,6 @@ if __name__ == '__main__':
     plt.xlabel('학기', fontdict=font2, labelpad=10)
     #plt.ylabel('성적', fontdict=font3)
 
-    excelData = ExcelData()
-    processData = ProcessData()
-
-    zum = []
     gradeCount = excelData.getGradeCount()  # 총 이수 학기 획득
     for i in range(gradeCount): # 이수 학기 만큼 평균 학점 계산
         zum.append(processData.getAvg(i))
@@ -50,5 +57,7 @@ if __name__ == '__main__':
         str_val = '%.2f' % zum[i]
         plt.text(v, zum[i], str_val, fontsize=10, color='#ff0000',
                     horizontalalignment='center', verticalalignment='bottom')
+
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
     plt.show()
